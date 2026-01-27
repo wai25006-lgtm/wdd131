5bfdad57d630af9a0195f8429b2e32f02aa29c2d
wai25006-lgtm <wai25006@byui.edu>
2026-01-26 19:08:44 -0700

    Responsive

---
+++ ponder/week-three/responsive.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector(".menu-btn");
+
+let menu = document.querySelector("nav");
+
+btn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
