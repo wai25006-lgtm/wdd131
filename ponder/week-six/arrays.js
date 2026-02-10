dc40bf86e967e92f59bc175d9b2e81228e0fc3cd
wai25006-lgtm <wai25006@byui.edu>
2026-02-09 18:19:47 -0700

    arrays

---
+++ ponder/week-six/arrays.js
@@ -0,0 +1,90 @@
+let pets = ['goldfish', 'dog', 'rhino'];
+// let pet = newArray('goldfish', 'dog', 'rhino')
+
+console.log(pets.length);
+console.log(pets[2]);
+
+pets[3] = 'bunny';
+console.log(pets);
+
+pets.push('lizard')
+console.log(pets)
+
+const steps = ['one', 'two', 'three'];
+//.forEach call a function for every item in the array
+
+steps.forEach(function(item) {
+    console.log(item)
+})
+
+steps.forEach(showSteps);
+
+function showSteps(item) {
+    console.log(item) //can replace item with step, x, or other
+}
+
+let myList = document.querySelector('#myList')
+
+//.map also calls a function but creates a new array from the original array
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item) {
+    return `<li>${item}</li>}`
+}
+
+myList.innerHTML = stepsHtml.join('');
+//.map
+
+let grades = ['A', 'B', 'D'];
+
+let points;
+
+let gpaPoints = grades.map(convert);
+
+function convert(grade){
+    switch (grade.toUpperCase()) {
+        case 'A': 
+            points = 4;
+            break
+        case 'B':
+            points = 3;
+            break
+        case 'C':
+            points = 2;
+            break
+        case 'D':
+            points = 1;
+            break
+        case 'F':
+            points = 0;
+            break
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+console.log(gpaPoints);
+console.log(grades);
+
+//.reduce - reduce down to a single value with an accumulator
+
+let totalPoints = gpaPoints.reduce(getTotal)
+
+function getTotal(total, item) {
+    return total + item;
+}
+
+console.log(totalPoints);
+let gpaAverage = totalPoints/gpaPoints.length;
+
+console.log(gpaAverage);
+
+//.filter - make a new array but only items that pass a certain 
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+         
+const shortWords = words.filter(function(word) {
+    return word.length <= 6;
+})
+
+console.log(shortWords);
