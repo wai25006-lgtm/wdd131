15f0d4e651722fe5ed41cc3dcc9484f11fb57e84
wai25006-lgtm <wai25006@byui.edu>
2026-02-17 21:10:14 -0700

    form

---
+++ ponder/week-five/blog/blog.js
@@ -0,0 +1,61 @@
+const articles = [
+	{
+		id: 1,
+		title: 'Septimus Heap Book One: Magyk',
+		date: 'July 5, 2022',
+		description:
+			'If you enjoy stories about seventh sons of seventh sons and magyk this is the book for you.',
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/en/5/5f/Magkycover2.jpg',
+		imgAlt: 'Book cover for Septimus Heap 1',
+		ages: '10-14',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 2,
+		title: 'Magnus Chase Book One: Sword of Summer',
+		date: 'December 12, 2021',
+		description:
+			'The anticipated new novel by Rick Riordan. After Greek mythology (Percy Jackson), Greek/Roman (Heroes of Olympus), and Egyptian (Kane Chronicles), Rick decides to try his hand with Norse Mythology, and the end result is good.',
+		imgSrc:
+			'https://books.google.com/books/content/images/frontcover/xWuyBAAAQBAJ?fife=w300',
+		imgAlt: 'Book cover for Magnus Chase 1',
+		ages: '12-16',
+		genre: 'Fantasy',
+		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+	}
+];
+     
+const bookList = document.getElementById('book-list');
+
+articles.forEach(article => {
+
+    const bookArticle = document.createElement('article');
+    bookArticle.className = 'book';
+
+    let html = `
+    <h2>${article.title}</h2>
+    <img src="${article.imgSrc}" alt="${article.imgAlt}">
+    <p><strong>Date:</strong> ${article.date}</p>
+    <p><strong>Ages:</strong> ${article.ages}</p>
+    <p><strong>Genre:</strong> ${article.genre}</p>
+    <p><strong>Rating:</strong> <span aria-label="${article.stars.length} out of 5 stars" role="img">${article.stars}</span></p>
+    <p id="desc">${article.description}</p>
+    `;
+
+    bookArticle.innerHTML = html;
+    bookList.appendChild(bookArticle);
+});
