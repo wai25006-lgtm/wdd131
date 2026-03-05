640ffbc22244089a2b811ff72e5aefbf51eef3d9
wai25006-lgtm <wai25006@byui.edu>
2026-03-04 23:39:56 -0700

    flexbox

---
+++ ponder/week-nine/script.js
@@ -0,0 +1,10 @@
+class Character {
+    constructor(name, charClass, level, health, image) {
+        this.name = name;
+        this.charClass = charClass;
+        this.level = level;
+        this.health = health;
+        this.image = image;
+    }
+}
+
